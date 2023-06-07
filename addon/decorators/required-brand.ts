76b0e6ced42d42541551b9468de19862b800cf1c
MaximilienBrunelle <maximilien.brunelle@gmail.com>
2023-06-07 17:45:03 +0200

    Removed: unused comment

--- addon/decorators/required-brand.ts
+++ addon/decorators/required-brand.ts
@@ -28,20 +28,3 @@ export function requiredBrand(brand: string, fallbackRoute: string) {
     }
   };
 }
-
-// export function logConstruction(actionDescription: string, actionType: ActivityType) {
-//   return function extendClass<T extends Constructable>(BaseClass: T) {
-//     return class extends BaseClass {
-//       constructor(...args: any) {
-//         super(...args);
-//         assert(
-//           '[decorator][log-construction] An actionDescription needs to be passed for the activity-log to make sense.',
-//           typeof actionDescription === 'string'
-//         );
-//         getOwner(this)
-//           .lookup('service:activity-tracking')
-//           .log(actionType ?? 'page_view', actionDescription);
-//       }
-//     };
-//   };
-// }
